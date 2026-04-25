bc904fae75d5cf0936c9ea23e1679581e284ca77
Hsu1991 <ac12cb3a2002@gmail.com>
2026-04-25 21:53:22 +0800

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        // this fails at [0,0,0]
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ret;
+        int len = nums.size();
+        int prev = 0;
+        for (int i = 0; i < len; i++)
+        {
+            if(i != 0 && nums[i] == prev)
+                // duplicates
+                continue;
+            vector<vector<int>> result = twosum(nums, i + 1, len, 0 - nums[i]);
+            for(auto jt = result.begin(); jt != result.end(); jt++)
+            {
+                (*jt).push_back(nums[i]);
+                ret.push_back(*jt);
+            }
+            prev = nums[i];
+        }
+        return ret;
+    }
+    vector<vector<int>> twosum (vector<int>& nums, int start, int end, int target)
+    {
+        vector<vector<int>> result;
+        unordered_map<int, int> tmp;
+        int prev = 0;
+        // cout << "target:" <<target << "\n";
+        for(int i = start; i < end; i++)
+        {
+            // cout << nums[i] << "\n";
+            if(tmp.contains(target - nums[i]) && tmp[target - nums[i]] == 0)
+            {
+                vector<int> valid_pair;
+                valid_pair.push_back(nums[i]);
+                valid_pair.push_back(target - nums[i]);
+                result.push_back(valid_pair);
+                // cout << "matched\n";
+                tmp[target - nums[i]] = tmp[target - nums[i]] + 1;
+            }
+            else
+            {
+                // insert nums[i] instead of target - nums[i] => 2nd times
+                tmp[nums[i]] = 0;
+            }
+            prev = nums[i];
+        }
+        return result;
+    }
+};
